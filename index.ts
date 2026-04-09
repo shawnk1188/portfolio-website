a37a1c48bee44733bc8407a2f9ab8c0c2f7af943
shawnk1188 <shawngit1188@gmail.com>
2026-04-09 18:19:55 -0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,60 @@
+export interface SiteConfig extends HeaderProps {
+  title: string;
+  description: string;
+  lang: string;
+  author: string;
+  socialLinks: { text: string; href: string }[];
+  socialImage: string;
+  canonicalURL?: string;
+}
+
+export interface SiteContent {
+  hero: HeroProps;
+  experience: ExperienceProps[];
+  projects: ProjectProps[];
+  skills: SkillsProps;
+  about: AboutProps;
+}
+
+export interface HeroProps {
+  name: string;
+  specialty: string;
+  summary: string;
+  email: string;
+}
+
+export interface ExperienceProps {
+  company: string;
+  position: string;
+  startDate: string;
+  endDate: string;
+  summary: string | string[];
+}
+
+export interface ProjectProps {
+  name: string;
+  summary: string;
+  image: string;
+  linkPreview?: string;
+  linkSource?: string;
+}
+
+export interface SkillCategoryProps {
+  title: string;
+  items: string[];
+}
+
+export interface SkillsProps {
+  categories: SkillCategoryProps[];
+  certifications: string[];
+}
+
+export interface AboutProps {
+  description: string;
+  image: string;
+}
+
+export interface HeaderProps {
+  siteLogo: string;
+  navLinks: { text: string; href: string }[];
+}
